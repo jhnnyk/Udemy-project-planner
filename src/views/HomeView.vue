aa29761946d0c4857468ab88aec445af13275dc5
John Knotts <jhnnyk@gmail.com>
2022-07-11 15:54:14 -0600

    single project component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <p>{{ project.title }}</p>
+        <SingleProject :project="project" />
       </div>
     </div>
   </div>
@@ -10,10 +10,11 @@
 
 <script>
 import data from '../data/db.json';
+import SingleProject from '../components/SingleProject.vue';
 
 export default {
   name: 'Home',
-  components: {},
+  components: { SingleProject },
   data() {
     return {
       projects: [],
