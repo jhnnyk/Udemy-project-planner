83748436f1ac87ab38382c67b45345e8047925e4
John Knotts <jhnnyk@gmail.com>
2022-07-11 15:41:14 -0600

    add project data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import data from '../data/db.json';
+
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    this.projects = data.projects;
+  },
 };
 </script>
