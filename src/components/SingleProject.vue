6b51a9a6468a9f9f1f4b83b6bfb028915cb807b2
John Knotts <jhnnyk@gmail.com>
2022-07-12 18:05:52 -0600

    using material icons

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="toggleShowDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-symbols-outlined">edit</span>
+        <span class="material-symbols-outlined">delete</span>
+        <span class="material-symbols-outlined">done</span>
+      </div>
     </div>
     <div class="details" v-if="showDetails">
       <p>
@@ -39,5 +44,23 @@ export default {
 
 h3 {
   cursor: pointer;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.material-symbols-outlined {
+  font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48; /* per Google's instructions */
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-symbols-outlined:hover {
+  color: #777;
 }
 </style>
